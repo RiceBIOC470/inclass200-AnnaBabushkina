ffe300f6995dde2452504c1aa4ff163dd5291bbb
warmflasha <warmflasha@gmail.com>
2019-11-13 22:14:38 -0600

    Added inclass20

---
+++ Inclass20.m
@@ -0,0 +1,15 @@
+%Inclass 20
+
+%In this folder you will find a .mat file with x and y data
+
+% For all of the questions below, use any options as necessary to get a
+% good fit - starting points, lower bounds etc. 
+
+% 1. Define a custom model and try to fit the data to a Michaelis function 
+% y = V*x/(K+x). 
+% 2. Use a custom model to try to fit the data to a Hill function with
+% exponent 2: y = V*x^2/(K^2+x^2)
+% 3. Now treat the exponent as a free parameter and perform the fit again,
+% that is fit to the model y = V*x^n/(K^n+x^n)
+% 4. Redo parts 1 and 2, but using your model from 3 and fixing the value of
+% n to either 1 or 2.
